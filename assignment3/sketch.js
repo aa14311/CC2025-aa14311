c3aaedcdeb7da2f132654b6be912584ddae89680
aa14311 <aa14311@nyu.edu>
2025-10-08 11:53:12 -0400

    Update sketch.js

--- assignment3/sketch.js
+++ assignment3/sketch.js
@@ -1,6 +1,6 @@
-//defined time variables for (1) flower growth (2) petal color change.
-//have not defined a third time variable, but will add a regrowth
-//time variable after x amount of time.
+//time variables for (1) flower growth (2) petal color change, which will
+//need adjusting. Have not defined a third time variable, but will add a regrowth
+//time variable after x amount of time in addition to adding more flowers.
 let petalColor
 let growth = 0
 let startTime
@@ -8,13 +8,14 @@ let startTime
 function setup() {
   createCanvas(windowWidth, windowHeight);
   noStroke();
-  //setting transparency and minimum color variables to avoid dark tones. 
-  petalColor = color(random(100, 255), random(100, 255), random(100, 255), 220)
+  //setting transparency and maximum color variables to avoid lighter flower petal tones
+  //to the light background
+  petalColor = color(random(0, 155), random(0, 155), random(0, 155), 200);
   startTime = millis();
 }
 
 function draw() {
-  background(0);
+  background("#c9b197ff");
   //calling the flower from the flower function below
   flower();
 }
@@ -28,15 +29,17 @@ function flower() {
 
   //changes petal color every 3 seconds and avoid growth time at start
   if(elapsed % 3 ==0 && elapsed != 0){
-    petalColor = color(random(100, 255), random(100, 255), random(100, 255), 220);
+  petalColor = color(random(0, 130), random(0, 130), random(0, 130), 200);
   }
 
   //flower code from p5js site
    for (var r31 = 0; r31 < 10; r31++) {
-    stroke(85,107,47,20);
+    stroke("#4b7533ff");
     strokeWeight(5);
+    //adjusting time of 
     if (seconds <= 10) {
-      line(180, 535, 180, 390 + frameCount / 10);
+      //adjusted parameters for the stem to decrease along with the petal growth
+      line(180, 535, 180, 390 + frameCount / 13);
     }
     else {
       line(180, 535, 180, 435);
@@ -49,6 +52,7 @@ function flower() {
   translate(180, 385);
   noStroke();
   for (var r3 = 0; r3 < 10; r3++) {
+    //will adjust the timing and growth size in final version.
     if (frameCount <= 600) {
       ellipse(0, 10 + frameCount / 20, 10 + frameCount / 40, 20 + frameCount / 20);
     }
