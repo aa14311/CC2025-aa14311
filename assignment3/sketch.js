5101d7fe83cc85e5f98e8b473ca53a14fff25121
aa14311 <aa14311@nyu.edu>
2025-10-08 12:11:52 -0400

    Update sketch.js

--- assignment3/sketch.js
+++ assignment3/sketch.js
@@ -2,6 +2,7 @@
 //need adjusting. Have not defined a third time variable, but will add a regrowth
 //time variable after x amount of time in addition to adding more flowers.
 let petalColor
+let petalColor2 // added to create different petal colors for additional flowers, not yet implemented.
 let growth = 0
 let startTime
 
@@ -36,7 +37,7 @@ function flower() {
    for (var r31 = 0; r31 < 10; r31++) {
     stroke("#4b7533ff");
     strokeWeight(5);
-    //adjusting time of 
+    //will need to adjust
     if (seconds <= 10) {
       //adjusted parameters for the stem to decrease along with the petal growth
       line(180, 535, 180, 390 + frameCount / 13);
@@ -50,6 +51,35 @@ function flower() {
   push();
   fill(petalColor);
   translate(180, 385);
+  noStroke();
+  for (var r3 = 0; r3 < 10; r3++) {
+    //will adjust the timing and growth size in final version.
+    if (frameCount <= 600) {
+      ellipse(0, 10 + frameCount / 20, 10 + frameCount / 40, 20 + frameCount / 20);
+    }
+    if (frameCount > 600) {
+      ellipse(0, 40, 25, 50)
+    }
+    rotate(PI / 5);
+  }
+  pop();
+
+
+  //flower #2 copied code from above
+  for (var r31 = 0; r31 < 10; r31++) {
+    stroke("#4b7533ff");
+    strokeWeight(5);
+    if (seconds <= 10) {
+      //adjusted parameters for the stem to decrease along with the petal growth
+      line(380, 535, 380, 390 + frameCount / 13);}
+      else { line(380, 535, 380, 435);
+    }
+    noStroke();
+  }
+  
+  push();
+  fill(petalColor);
+  translate(380, 385);
   noStroke();
   for (var r3 = 0; r3 < 10; r3++) {
     //will adjust the timing and growth size in final version.
