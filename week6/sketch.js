989410516057ac4b902bb80f387c0eccca5226d0
aa14311 <aa14311@nyu.edu>
2025-10-17 13:46:04 -0400

    Update sketch.js

--- week6/sketch.js
+++ week6/sketch.js
@@ -1,52 +1,52 @@
+let drunks = [];// square brackets indicate i'm making
+let drunkAmount = 1000;
+//an array
+
+// an array is a variable that contains multiple variables
+// each individual variable can be accessed using an 
+// index number that is fed into the square brackets
+// like so: drunks[5] would give me the 6th drunk in the list
+
+
 function setup() {
   createCanvas(windowWidth, windowHeight);
-  angleMode(DEGREES);
+  background(100,180,130);
+  x = width/2;
+  y = height/2;
+  colorMode(HSB);
+
+  for(let i = 0;i<drunkAmount;i++){
+    let drunkD = random(10,50); // diamters from 10 to 100
+    let drunkSpeed = random(1,7); // speeds from 1 to 7
+    let drunkHue = random(0,60); // hues from 0 to 60
+    drunks[i] = new Drunk(width/2,height/2,drunkD,drunkSpeed,drunkHue);
+  }
 }
 
 function draw() {
-  background("#c9b197ff");
+  for(let i = 0;i<drunks.length;i++){
+    drunks[i].move();
+    drunks[i].display();
+  }
+}
 
-  let h = hour();
-  let m = minute();
-  let s = second();
-  let ms = millis() % 1000;
+class Drunk { // class declares a new type of object
+  constructor(x,y,diameter,speed,hue){
+    this.x = x;
+    this.y = y;
+    this.diameter = diameter;
+    this.speed = speed;
+    this.hue = hue;
+    this.opacity = random(0,1); // you can also initialize variables here
+  }
 
-  // Seconds rectangle (bottom → top)
-  fill("#ab6d2bff");
-  noStroke();
-  let sSmooth = s + ms / 1000;
-  let sY = map(sSmooth, 0, 60, height, 0);
-  rect(150, sY, 100, 20);
+  move(){ // you can declare functions or "methods" like this
+    this.x = this.x+random(-this.speed,this.speed);
+    this.y = this.y+random(-this.speed,this.speed);
+  }
 
-  stroke(0);
-  strokeWeight(1);
-  line(150,0,150,height);
-
-  stroke(0);
-  strokeWeight(1);
-  line(250,0,250,height);
-
-  let loopTime = 2000; 
-  fill("#f19634ff");
-  noStroke();
-  let sY1 = map((millis() % loopTime) / 20, 0, 80, height, 0);
-  rect(200, sY1, 100, 20);
- 
-  fill("#754511ff");
-  noStroke();
-  let sY2 = map((millis() % loopTime) / 15, 0, 80, height, 0);
-  rect(400, sY2, 100, 20);
-  
-  // Minutes rectangle
-  fill("#846442ff");
-  let mY = map(m + s / 60, 0, 60, height, 0);
-  rect(250, mY, 100, 20);
-
-  // Hours rectangle
-  fill("#5c3f20ff");
-  let hY = map(h % 12 + m / 60, 0, 12, height, 0);
-  rect(350, hY, 100, 20);
-
-  
-
+  display(){
+    fill(this.hue,70,100,this.opacity);
+    circle(this.x,this.y,this.diameter);
+  }
 }
