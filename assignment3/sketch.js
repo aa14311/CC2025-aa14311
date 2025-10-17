473f2ec60aa8c7912ffd49fc73791a06e7411a76
aa14311 <aa14311@nyu.edu>
2025-10-17 13:41:28 -0400

    assignment 3 updates / started week 7 notes during virtual lecture but listened

--- assignment3/sketch.js
+++ assignment3/sketch.js
@@ -1,82 +1,78 @@
-
+//global variables added with you during our 1:1 to update
+//the seconds block to stop the ticking every second.
+let prevMs = 0;
+let prevS = 0;
 
 function setup() {
   createCanvas(windowWidth, windowHeight);  
 }
 
 function draw() {
-  background("#c9b197ff");
+  //updated colors back to your color pallette resources in week 3.
+  background("#EEE3CB");
+  if(second() != prevS){
+    //added variables created with you during 1:1, to fix the ticking
+    //movement of the seconds block. 
+    prevS = second(); // resets the timer 
+    prevMs = millis();
+  }
 
+  //defined the variables that will represent the abstract clock
+  //of floating blocks below
   let h = hour();
   let m = minute();
   let s = second();
-  let ms = millis() % 1000;
+  //updated with global variable to get smooth transition of floating block. 
+  let ms = millis() - prevMs;
+
+  //millisecondsBlock. Also created with you to represent each second
+  //elapsed for the seconds block. I chose the 8 stroke size due to the
+  //simplicity of the abstract clock and thought it provided more emphasis
+  //and cohesive separation on the moving blocks.
+  stroke(8);
+  fill("#9BABB8");
+  //updated parameters to show each time variable aligning with the page height
+  //so they all reset from bottom to top. 
+  let msBlock = map((ms), 0, 1000, height, 0);
+  //time blocks created with x and y parameters to align with dynamic window
+  //size changes. I liked the rectangle height of 80 to represent the proportions
+  //to the entire page/canvas size. 
+  rect(width * 0, msBlock, width/4, 80);
 
   //seconds block
-  fill("#ab692bff");
-  noStroke();
-  stroke(3);
+  fill("#D7C0AE");
+  stroke(8);
+  //smooth variable created to avoid the rectangles moving in a ticking manner. Adding
+  //both time variables needed for smooth transitions.
   let sSmooth = s + ms / 1000;
+  //the youtube video that helped me better understand the map function: https://www.youtube.com/watch?v=nicMAoW6u1g
   let sBlock = map(sSmooth, 0, 60, height, 0);
-  rect(700, sBlock, 200, 80);
+  rect(width * 0.25, sBlock, width/4, 80);
 
   //minutes block
-  fill("#846442ff");
+  fill("#967E76");
   let mBlock = map(m + s / 60, 0, 60, height, 0);
-  rect(900, mBlock, 200, 80);
+  rect(width * 0.5, mBlock, width/4, 80);
 
   //hours block
-  fill("#5c3f20ff");
+  fill("#da9f72ff");
   let hBlock = map(h % 12 + m / 60, 0, 12, height, 0);
-  rect(1100, hBlock, 200, 80);
+  rect(width * 0.75, hBlock, width/4, 80);
 
-  //block1
-  let loopTime = 2000; 
-  stroke(3);
-  fill("#cfa578ff");
-  let block1 = map((millis() % loopTime) / 20, 0, 80, height, 0);
-  rect(100, block1, 200, 80);
-
-  //block2
-  let loopTime2 = 4000;
-  fill("#d08d46ff");
-  let block2 = map((millis() % loopTime2) / 40, 0, 80, height, 0);
-  rect(300, block2, 200, 80);
-
-  //block3
-  let loopTime3 = 6000;
-  fill("#c98134ff");
-  let block3 = map((millis() % loopTime3) / 60, 0, 80, height, 0);
-  rect(500, block3, 200, 80);
-
+  //lanes created to distinguish each time block using the same stroke
+  //weight and width/height parameters for dynamic window size changes.
   stroke(0);
-  strokeWeight(5);
-  line(100,0,100,height);
+  strokeWeight(8);
+  line(width * 0.25, 0, width * 0.25, height);
+  line(width * 0.5, 0, width * 0.5, height);
+  line(width * 0.75, 0, width * 0.75, height);
 
-  stroke(0);
-  strokeWeight(5);
-  line(300,0,300,height);
-
-  stroke(0);
-  strokeWeight(5);
-  line(500,0,500,height);
-
-  stroke(0);
-  strokeWeight(5);
-  line(700,0,700,height);
-
-  stroke(0);
-  strokeWeight(5);
-  line(900,0,900,height);
-
-  stroke(0);
-  strokeWeight(5);
-  line(1100,0,1100,height);
-
+  //text for added display and to show more clearly how the abstract clock
+  //functions.
   fill("#000000");
   noStroke();
-  text('hour: ' + hour(),10,20);
-  text('minute: ' + minute(),10,40);
-  text('second: ' + second(),10,60);
-  pop()
+  textSize(15);
+  text('hour: ' + hour(),5,20)
+  text('minute: ' + minute(),5,50);
+  text('second: ' + second(),5,80);
 }
