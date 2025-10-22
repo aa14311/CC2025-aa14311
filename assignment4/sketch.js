c5f268da1e2ca0dac1560bf5662dc47fcafb1561
aa14311 <aa14311@nyu.edu>
2025-10-22 12:48:04 -0400

    assignment 4 final updates and commentary

--- assignment4/sketch.js
+++ assignment4/sketch.js
@@ -1,3 +1,8 @@
+//Assignment 4: Object Factory - I chose a flower as my object
+//and it changes per click with # of petals, color, and size. The
+//composition in the code closely follows to your Pizza recording.
+
+//updated to an arry to account for multiple flowers.
 let myFlowers = [];
 
 function setup() {
@@ -5,18 +10,32 @@ function setup() {
 }
 
 function draw() {
-  background('#f3c5c5ff');
+  background('#e6cacaff');
+  //for loop with .length and [i] to account for every element in the array to
+  //display in conjunction with the myFlowers.push in mousePressed below.
   for(let i = 0; i < myFlowers.length; i++){
     myFlowers[i].display();
   }
 }
 
 function mousePressed(){
-  let flowerSize = random([.5, 1, 1.5]);
-  let tempFlower = new Flower(mouseX, mouseY,200,2,random(8,16),flowerSize);
+  //flowerSize was added as my 3rd dynamic change per click and set
+  //the parameters to the following sizes so the range of # of petals
+  //look aesthetic to each size (vice versa).
+  let flowerSize = random([.5, .75, 1, 1.25, 1.5]);
+  let tempFlower = new Flower(mouseX, mouseY,200,2,random(10,20),flowerSize);
   myFlowers.push(tempFlower);
 }
+//class Flower was added after moving my static drawing and variables originally
+//from the draw and setup functions into an object oriented manner as they group
+//variables and functions needed for the dynamic/addition of flowers per click.
+//I followed your pizza recording to get the main composition of the code below
+//including the constructor, variables, arrays, and for loop. 
 
+//additional declaration of arrays were added to account for the dynamic changes
+//with petalColor and flowerSize (in addition to # of petals). I originally set the
+//colors with darker tones, but liked the overall composition of the bigger range in
+//colors with the background set with the slight opacity setting. 
 class Flower {
   constructor(x,y,width,height,numberOfPetals,flowerSize){
     this.x = x;
@@ -26,10 +45,17 @@ class Flower {
     this.numberOfPetals = numberOfPetals;
     this.petalPosition = [];
     this.petalRotation = [];
-    this.petalColor = [];
-    this.petalColor = color(random(25, 255), random(25, 255), random(25, 255));
+    this.petalColor = color(random(25, 255), random(25, 255), random(25, 255), 235);
     this.flowerSize = flowerSize;
 
+    //Like in your recording, the "360/" in spacing to account for the circular shape
+    //of the ellipses to represent a flower/petals. Since ellipses have
+    ///width and height variables, I liked the size of the "/4" division
+    //to my desired size in conjunction with # of petals.
+
+    //"flowerSize" variable was added as a multiplier last to get my 3rd dynamic
+    //change per click. petalRotation was also added to have the ellipses/petals
+    //face outwards. 
     for(let i = 0; i<this.numberOfPetals; i++){
       let spacing = 360/this.numberOfPetals;
       let petalX = cos(radians(i*spacing))*((this.width/4) * (this.height/4)) * this.flowerSize;
@@ -40,14 +66,17 @@ class Flower {
     }
   }
 
+//moved the petals originally written in the draw function and added the static stem
+//and leaves with the push and pop to anchor. 
 display(){
 
-  //stem
+  //stem - liked the different green color in contrast with the leaves. Also used
+  //the translate for positioning and rotate for the slight upward facing of leaves.
   push();
   translate(this.x,this.y);
-  stroke('#167a1aff');
-  strokeWeight(4);
-  line(0,this.height*6,0,100);
+  stroke('#11a616ff');
+  strokeWeight(5);
+  line(0,this.height*10,0,100);
 
   //leaf
   push();
@@ -76,7 +105,10 @@ display(){
   ellipse(11,this.height*30,20,8);
   pop();
 
-  //flower petals 
+  //Again similar to your example, the for loop originally from the draw function to
+  //the display function to add number of petals and pulling the x and y
+  //coordinates and rotation stored in the for loop now in the class function.
+  //Rotation was added so the ellipses face outwards from the center.
   for(let i = 0; i<this.numberOfPetals; i++){
     push();
     fill(this.petalColor);
